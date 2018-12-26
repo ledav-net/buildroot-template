3b5b98ac0450f3b01086cb7593da695d3dc0bfeb
David De Grave <david@ledav.net>
2018-12-26 18:21:47 +0100

    scripts/: Merge of changes from bbb-station project ...
    [489]: Let the script go in working dir by itself
           Now that script can be called using the absolute path. It will
           change the cwd right before includes & processing...
    [485]: Use of "pv" if there is one installed on the system.
           Otherwise, fall-back to the old scenario...
    [472]: Fixed an issue in flash.sh -u ...
    [469]: Add option -u
           To update the sd card that contain already an image. This to speed
           up the process.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,14 +2,25 @@
 
 [ $UID -eq 0 ] || exec sudo "$0" "$@"
 
+cd $(dirname `readlink -f $0`)
+
 . include/log || exit 1
 . include/functions || exit 1
 
 device=
+update=0
+image=sdcard.img
+pv=$(which pv 2>/dev/null)
 
-[ -b "$1" ] || die "Usage: $0 block_device"
+if [ "$1" = "-u" ]; then
+	update=1
+	shift
+fi
+
+[ -b "$1" ] || die "Usage: $0 [-u] block_device"
+
 device=$1
-[ -s "sdcard.img" ] || die "Cannot find 'sdcard.img' !"
+[ -s $image ] || die "Cannot find '$image' !"
 
 print_device_table $device
 confirm
@@ -19,6 +30,35 @@ for mnt in $(grep $device /proc/mounts | cut -f1 -d' '); do
 	umount $mnt || die "Cannot unmount $mnt !"
 done
 
-dd if="sdcard.img" of="$device" oflag=sync bs=4M status=progress && sync
+if [ $update -eq 1 ]
+then
+	log "Updating existing partitions ..."
+	lm=$(mktemp -d)
+	dm=$(mktemp -d)
+	ld=$(losetup --show -f -P "$image")
+	mkdir -p "$lm" "$dm"
+	for p in 1 2
+	do
+		log -- "- Syncing $image partition $p on $(ls ${device}*$p) ..."
+		mount ${ld}p$p "$lm" || die "mount error !"
+		mount ${device}*$p "$dm" || die "mount error !"
+		rsync --inplace -a -u "$lm"/ "$dm"/ || die "rsync error !"
+		umount "$lm" "$dm" || die "umount error !"
+	done
+	losetup -d "$ld"
+	rmdir "$lm" "$dm"
+else
+	log "Flashing $image on $device ..."
+	if [ -z "$pv" ]; then
+		dd if="$image" of="$device" oflag=sync bs=4M status=progress
+	else
+		dd if="$image" bs=8M status=none \
+		| $pv -w80 -s $(du -s -b sdcard.img | cut -f1) \
+		| dd of="$device" oflag=sync bs=4M status=none
+	fi
+	sync
+fi
+
+log "Done!"
 
 exit 0
